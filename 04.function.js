f2ccea91822bb8945548248395c84836409e31ea
Yelihi <Yelihi19@gmail.com>
2022-06-24 10:00:41 +0900

    number 5

--- 04.function.js
+++ 04.function.js
@@ -1,2 +1,11 @@
-/* hello */
-console.log('git test');
+/* hello 
+함수를 실행 시키는 것을 호출이라 합니다.
+함수의 선언과 호출의 차이를 알아야 합니다. 
+
+*/
+function checkYear(){
+    return new Date().getFullYear;
+}
+
+checkYear();
+// 함수를 선언하고 호출하였습니다.
---
+++ 05.function_return.js
@@ -0,0 +1,23 @@
+/* 
+return 값은 함수의 결과값이라고 생각하면 됩니다. 
+즉 함수를 실행하였을 때 함수가 내놓는 결과값이기에 변수에 저장이 가능합니다.
+
+괄호 안에 변수가 들어가있는데 이것을 매개변수라 합니다. 매개변수는 말그대로 변수입니다. 
+나중에 배우겠지만 매개변수도 변수로 취급해야합니다. 
+
+function add(a, b){
+    return a + b
+}
+
+fucntion add(){
+    let num1 = a;
+    let num2 = b;
+    return num1 + num2
+}
+이런 뜻 입니다.
+
+*/
+
+function addTen(num){
+    return num + 10;
+};
